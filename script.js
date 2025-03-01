b41b11abc2159efc810498a9c197bc210e237fed
rambabu1458 <58924997+rambabu1458@users.noreply.github.com>
2025-03-01 11:22:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //your JS code here. If required.
 
-// let button=document.getElementsByTagName("input");
-// button[0].addEventListener("click", function () {
-//       let task=document.getElementById("colorSelect");
-//       // let list=document.getElementsByTagName("option");
-// 	 let input=task.selectedIndex;
-//      task.remove(input);
-// });
-document.querySelector("input").addEventListener("click", function () {
-    let task = document.querySelector("#colorSelect");
-    let inputId = task.selectedIndex;
-    task.remove(inputId);
+let button=document.getElementsByTagName("input");
+button[0].addEventListener("click", function () {
+      let task=document.getElementById("colorSelect");
+      // let list=document.getElementsByTagName("option");
+	 let input=task.selectedIndex;
+     task.remove(input);
 });
+// document.querySelector("input").addEventListener("click", function () {
+//     let task = document.querySelector("#colorSelect");
+//     let inputId = task.selectedIndex;
+//     task.remove(inputId);
+// });
